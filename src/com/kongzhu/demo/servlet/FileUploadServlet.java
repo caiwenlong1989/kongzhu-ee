cde502d13e259247cdf6e00f91fd1f459173fe6d
caicai <594665291@qq.com>
2017-07-12 21:44:51 +0800

    上传文件超限处理

--- src/com/kongzhu/demo/servlet/FileUploadServlet.java
+++ src/com/kongzhu/demo/servlet/FileUploadServlet.java
@@ -17,6 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import org.apache.tomcat.util.http.fileupload.FileUploadBase.FileSizeLimitExceededException;
+
 @SuppressWarnings("serial")
 @WebServlet("/FileUploadServlet")
 // 添加该注释，提供对上传文件的支持
@@ -31,43 +33,50 @@ public class FileUploadServlet extends HttpServlet {
         
         PrintWriter out = resp.getWriter();
         
-        // 获取form表单中的文件域
-        Part part = req.getPart("headImg");
-        
-        // 获取上传文件的名称
-        String disposition = part.getHeader("Content-Disposition");
-        String srcFileName = disposition.substring(
-                disposition.indexOf("filename") + 10, disposition.lastIndexOf("\""));
-        
-        // 检查上传文件类型
-        String extension = srcFileName.substring(srcFileName.indexOf("."));
-        List<String> extensions = new ArrayList<>(Arrays.asList(".jpg", ".jpeg", ".png"));
-        if (!extensions.contains(extension)) {
-            out.write("上传文件类型错误！目前仅支持JPG、JPEG、PNG。");
-            return;
+        try {
+            
+            // 获取form表单中的文件域
+            Part part = req.getPart("headImg");
+            
+            // 获取上传文件的名称
+            String disposition = part.getHeader("Content-Disposition");
+            String srcFileName = disposition.substring(
+                    disposition.indexOf("filename") + 10, disposition.lastIndexOf("\""));
+            
+            // 检查上传文件类型
+            String extension = srcFileName.substring(srcFileName.indexOf("."));
+            List<String> extensions = new ArrayList<>(Arrays.asList(".jpg", ".jpeg", ".png"));
+            if (!extensions.contains(extension)) {
+                out.write("上传文件类型错误！目前仅支持JPG、JPEG、PNG。");
+                return;
+            }
+            
+            // 设置存放上传文件的文件夹和名称
+            SimpleDateFormat dirFormat = new SimpleDateFormat("yyyyMMdd");
+            SimpleDateFormat fileFormat = new SimpleDateFormat("HHmmssS");
+            Date now = new Date();
+            String destDirName = "/Users/caiwenlong/Documents/" + dirFormat.format(now);
+            
+            // 如果文件夹不存在，需要手动创建
+            File dir = new File(destDirName);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            String destFileName = destDirName + "/" + fileFormat.format(now) + extension;
+            
+            // 将上传文件上传到指定位置
+            part.write(destFileName);
+            
+            // 设置响应信息
+            out.write("<br>文件上传成功！");
+            out.write("<br>上传文件名称：" + srcFileName);
+            out.write("<br>上传文件类型：" + part.getContentType());
+            out.write("<br>上传文件大小：" + part.getSize() + " 字节");
+        } catch (Exception e) {
+            if (e instanceof FileSizeLimitExceededException) {
+                out.write("上传文件大小超过最大值5M！");
+            }
         }
-        
-        // 设置存放上传文件的文件夹和名称
-        SimpleDateFormat dirFormat = new SimpleDateFormat("yyyyMMdd");
-        SimpleDateFormat fileFormat = new SimpleDateFormat("HHmmssS");
-        Date now = new Date();
-        String destDirName = "/Users/caiwenlong/Documents/" + dirFormat.format(now);
-        
-        // 如果文件夹不存在，需要手动创建
-        File dir = new File(destDirName);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        String destFileName = destDirName + "/" + fileFormat.format(now) + extension;
-        
-        // 将上传文件上传到指定位置
-        part.write(destFileName);
-        
-        // 设置响应信息
-        out.write("<br>文件上传成功！");
-        out.write("<br>上传文件名称：" + srcFileName);
-        out.write("<br>上传文件类型：" + part.getContentType());
-        out.write("<br>上传文件大小：" + part.getSize() + " 字节");
     }
 
 }
