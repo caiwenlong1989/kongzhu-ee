ac73dde11c4fa82a20a300dfc6675dff431a66ef
caicai <594665291@qq.com>
2017-07-13 16:53:11 +0800

    添加注释

--- src/com/kongzhu/demo/servlet/BatchFileDownloadServlet.java
+++ src/com/kongzhu/demo/servlet/BatchFileDownloadServlet.java
@@ -27,25 +27,30 @@ public class BatchFileDownloadServlet extends HttpServlet {
         // 文件以附件形式在客户端浏览器下载
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
         String zipName = sdf.format(new Date()) + ".zip";
-		resp.setHeader("Content-Disposition", "attachment; filename="
+        resp.setHeader("Content-Disposition", "attachment; filename="
                 // 如果文件名包含中文，则必须转码
                 + new String(zipName .getBytes(), "ISO-8859-1"));
         
         // 读写
-		String dirName = "C:/upload/20170712";
-		String[] fileNames = req.getParameterValues("fileName");
+        String dirName = "C:/upload/20170712"; // 目录名
+        String[] fileNames = req.getParameterValues("fileName"); // 客户勾选的下载文件名
+        
+        // ZIP压缩包文件流
         ZipOutputStream out = new ZipOutputStream(resp.getOutputStream());
         for (String fileName : fileNames) {
-        	ZipEntry e = new ZipEntry(fileName);
-			out.putNextEntry(e);
-			InputStream in = new FileInputStream(dirName + "/" + fileName);
-			int b = -1;
-			while ((b = in.read()) > -1) {
-				out.write(b);
-			}
-			in.close();
-			
-			out.closeEntry();
+            ZipEntry e = new ZipEntry(fileName); // 压缩包中的子项
+            out.putNextEntry(e); // 将子项放入压缩包
+            
+            // 读写文件
+            InputStream in = new FileInputStream(dirName + "/" + fileName);
+            int b = -1;
+            while ((b = in.read()) > -1) {
+                out.write(b);
+            }
+            in.close();
+            
+            // 文件流写完记得关闭子项，否则下次文件流会写入未关闭的子项中
+            out.closeEntry();
         }
         out.close();
     }
